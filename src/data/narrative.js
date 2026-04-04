bb6bb57cbd532094b9ce7cce825e8d093c58826a
Joe Jackson <joejackson@numiko.com>
2026-04-04 10:42:08 +0100

    begin narrative content

---
+++ src/data/narrative.js
@@ -0,0 +1,35 @@
+export const narrative = [
+  {
+    page: "00",
+    content: `
+      <h1>Prologue</h1>
+      <p>The Maze.</p>
+      <p>I met them at the gate though I usually wait inside. Preoccupied with their own thoughts, impatient, like so many children, they didn't see who I really was. They never noticed my crown, my pain, the fire in my eyes.</p>
+      <p>Like all the others they think the Maze was made for them; actually, it is the other way around. They think I am some poet who will lead them through the symbols and spaces of this Underworld. They think I will teach them lessons. They should call me Cerberus . . . I am the lesson.</p>
+      <p>The monstrous walls rise up and run away as far as the human eye can see, circling and dividing. Which half is the Maze?</p>
+      <p>Even I get lost. It changes-sometimes slowly, imperceptibly . . . sometimes suddenly. This House is not only made of stone and mortar, wood and paint; it is made of time and mystery, hope and fear. Construction never stops. I take some pride in my role as architect.</p>
+      <p>They think I will guide them to the center. Perhaps I will. . . .</p>
+      <p>The sun was very hot.</p>
+    `,
+    travel: {
+      enter: "",
+      exit: "Together we walked through the gate into",
+    },
+  },
+  {
+    page: "01",
+    content: `
+      <p>They looked carefully at the bronze doors, trying to choose. The uncertainty of visitors is one of my little pleasures.</p>
+      <p>"It's easy to get lost," I said helpfully. "This can be a sinister place." The sun glared at me through the gateway.</p>
+      <p>Something was ringing behind one of the doors. They spent some time trying to decide which door it was, not understanding that the silences of the Maze are as eloquent as the sounds.</p>
+      <p>"Decisions, decisions," one said. "Too many decisions."</p>
+      <p>"The story of my life," said another.</p>
+      <p>"We don't want to be late," said a third, opening one of the doors.</p>
+      <p>"Nary a soul to be seen," said the first, peering into the gloom.</p>
+    `,
+    travel: {
+      enter: "the entrance hall of the Maze.",
+      exit: "I waited patiently for them to choose which way to go . . . into",
+    },
+  },
+];
